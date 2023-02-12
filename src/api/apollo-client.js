7b688e361b04cccc698e632795b26b94a25fccb2
Lúcio Charallo <hi@lucio.app>
2023-02-12 17:29:03 -0300

    fix(mvp): api url

--- src/api/apollo-client.js
+++ src/api/apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://app.lucio.run/graphql',
+  uri: 'https://app.ivai.news/graphql',
   cache: new InMemoryCache(),
 });
 
