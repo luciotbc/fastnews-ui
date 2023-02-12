572717d93e8f6ad9dddc136ebbd86c45141e1da2
Lúcio Charallo <hi@lucio.app>
2023-02-12 19:18:56 -0300

    feat(GA): install Google Analitics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,28 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
+import { GA_TRACKING_ID } from '../utils/gtag';
+
 export default function Document() {
   return (
-    <Html lang='en'>
+    <Html lang='pt-BR'>
       <Head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+          `,
+          }}
+        />
         <link
           rel='preload'
           href='/fonts/inter-var-latin.woff2'
