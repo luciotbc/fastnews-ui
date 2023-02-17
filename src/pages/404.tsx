14713f7cf32ef6a2b92f4a810bb621653e27008d
Lúcio Charallo <hi@lucio.app>
2023-02-17 13:26:22 -0300

    fix(home): ops

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import * as React from 'react';
 import { RiAlarmWarningFill } from 'react-icons/ri';
 
@@ -17,9 +18,9 @@ export default function NotFoundPage() {
               className='drop-shadow-glow animate-flicker text-red-500'
             />
             <h1 className='mt-8 text-4xl md:text-6xl'>Page Not Found</h1>
-            <link className='mt-4 md:text-lg' href='/'>
-              Back to Home
-            </link>
+            <Link className='mt-4 md:text-lg' href='/'>
+              Voltar para o site
+            </Link>
           </div>
         </section>
       </main>
